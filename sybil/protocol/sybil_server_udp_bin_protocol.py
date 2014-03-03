32bacb191ea0242ad25594511019641a74f7c6b3
nidhogg <ismail.elouafiq@telecom-bretagne.eu>
2014-03-03 15:01:18 +0000

    Update UDP_serv_clie

--- sybil/protocol/sybil_server_udp_bin_protocol.py
+++ sybil/protocol/sybil_server_udp_bin_protocol.py
@@ -1,24 +1,187 @@
-# -*- coding: utf-8 -*-
-from twisted.internet.protocol import DatagramProtocol
-import ctypes
-import struct
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>/sg1/sybil/protocol/sybil_server_udp_bin_protocol.py - Changes - RES 302 Project for Group 14 - Redmine</title>
+<meta name="description" content="Redmine" />
+<meta name="keywords" content="issue,bug,tracker" />
+<meta content="authenticity_token" name="csrf-param" />
+<meta content="4z1nVhE2LsxW4nWCt1fpv81fXtrv18ZwjoE5zh1akE4=" name="csrf-token" />
+<link rel='shortcut icon' href='/redmine/favicon.ico?1374089464' />
+<link href="/redmine/stylesheets/jquery/jquery-ui-1.9.2.css?1374089464" media="all" rel="stylesheet" type="text/css" />
+<link href="/redmine/themes/TBTheme/stylesheets/application.css?1374090865" media="all" rel="stylesheet" type="text/css" />
 
-class SybilServerUdpBinProtocol(DatagramProtocol):
+<script src="/redmine/javascripts/jquery-1.8.3-ui-1.9.2-ujs-2.0.3.js?1374089464" type="text/javascript"></script>
+<script src="/redmine/javascripts/application.js?1374089464" type="text/javascript"></script>
+<script type="text/javascript">
+//<![CDATA[
+$(window).load(function(){ warnLeavingUnsaved('The current page contains unsaved text that will be lost if you leave this page.'); });
+//]]>
+</script>
 
-    def __init__(self, sybilBrain):
-        self.sybilBrain = sybilBrain
 
-    def datagramReceived(self, datagram, (host,port)):
-        print "received %r from %s:%d" % (raw_data, host, port)
-        length = len(datagram)
-        fmt = 'IH'+format(length)+'s'
-        raw_data = struct.unpack_from('IH'+fmt+'s',datagram[0:])
-        query = raw_data[6:]
-        response = self.sybilBrain.generateResponse(query)
+<!-- page specific tags -->
+  <script src="/redmine/javascripts/repository_navigation.js?1374089464" type="text/javascript"></script>
+<link href="/redmine/stylesheets/scm.css?1374089464" media="screen" rel="stylesheet" type="text/css" />
+</head>
+<body class="theme-Tbtheme controller-repositories action-changes">
+<div id="wrapper">
+<div id="wrapper2">
+<div id="wrapper3">
+<div id="top-menu">
+    <div id="account">
+        <ul><li><a href="/redmine/my/account" class="my-account">My account</a></li>
+<li><a href="/redmine/logout" class="logout" data-method="post" rel="nofollow">Sign out</a></li></ul>    </div>
+    <div id="loggedas">Logged in as <a href="/redmine/users/251" class="user active">ielouafi</a></div>
+    <ul><li><a href="/redmine/" class="home">Home</a></li>
+<li><a href="/redmine/my/page" class="my-page">My page</a></li>
+<li><a href="/redmine/projects" class="projects">Projects</a></li>
+<li><a href="http://www.redmine.org/guide" class="help">Help</a></li></ul></div>
 
-        buff = ctypes.create_string_buffer(6+length)
-        timeInSeconds = time.time()
-        struct.pack_into(fmt, buff, 0, timeInSeconds, length, line)
-        self.transport.write(buff, (self.server_host,self.server_port))
-     
+<div id="header">
+    <div id="quick-search">
+        <form accept-charset="UTF-8" action="/redmine/projects/r302-s14-g14/search" method="get"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div>
+        <input name="changesets" type="hidden" value="1" />
+        <label for='q'>
+          <a href="/redmine/projects/r302-s14-g14/search" accesskey="4">Search</a>:
+        </label>
+        <input accesskey="f" class="small" id="q" name="q" size="20" type="text" />
+</form>        <select id="project_quick_jump_box" name="project_quick_jump_box" onchange="if (this.value != &#x27;&#x27;) { window.location = this.value; }"><option value=''>Jump to a project...</option><option value="" disabled="disabled">---</option><option selected="selected" value="/redmine/projects/r302-s14-g14?jump=repository">RES 302 Project for Group 14</option></select>
+    </div>
 
+    <h1>RES 302 Project for Group 14</h1>
+
+    <div id="main-menu">
+        <ul><li><a href="/redmine/projects/r302-s14-g14" class="overview">Overview</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/activity" class="activity">Activity</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/issues" class="issues">Issues</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/issues/new" accesskey="7" class="new-issue">New issue</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/issues/gantt" class="gantt">Gantt</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/issues/calendar" class="calendar">Calendar</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/news" class="news">News</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/documents" class="documents">Documents</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/wiki" class="wiki">Wiki</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/files" class="files">Files</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/repository" class="repository selected">Repository</a></li>
+<li><a href="/redmine/projects/r302-s14-g14/settings" class="settings">Settings</a></li></ul>
+    </div>
+</div>
+
+<div id="main" class="nosidebar">
+    <div id="sidebar">
+        
+        
+    </div>
+
+    <div id="content">
+        
+        
+
+<div class="contextual">
+  
+<a href="/redmine/projects/r302-s14-g14/repository/statistics" class="icon icon-stats">Statistics</a>
+
+<form accept-charset="UTF-8" action="/redmine/projects/r302-s14-g14/repository/changes/sg1/sybil/protocol/sybil_server_udp_bin_protocol.py" id="revision_selector" method="get"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div>  <!-- Branches Dropdown -->
+
+
+    | Revision: 
+    <input id="rev" name="rev" size="8" type="text" />
+</form>
+</div>
+
+<h2><a href="/redmine/projects/r302-s14-g14/repository">root</a>
+    / <a href="/redmine/projects/r302-s14-g14/repository/show/sg1">sg1</a>
+    / <a href="/redmine/projects/r302-s14-g14/repository/show/sg1/sybil">sybil</a>
+    / <a href="/redmine/projects/r302-s14-g14/repository/show/sg1/sybil/protocol">protocol</a>
+    / <a href="/redmine/projects/r302-s14-g14/repository/changes/sg1/sybil/protocol/sybil_server_udp_bin_protocol.py">sybil_server_udp_bin_protocol.py</a>
+
+
+</h2>
+
+
+<p>
+History |
+    <a href="/redmine/projects/r302-s14-g14/repository/entry/sg1/sybil/protocol/sybil_server_udp_bin_protocol.py">View</a> |
+    <a href="/redmine/projects/r302-s14-g14/repository/annotate/sg1/sybil/protocol/sybil_server_udp_bin_protocol.py">Annotate</a> |
+<a href="/redmine/projects/r302-s14-g14/repository/raw/sg1/sybil/protocol/sybil_server_udp_bin_protocol.py">Download</a>
+(945 Bytes)
+</p>
+
+
+
+
+
+
+<form accept-charset="UTF-8" action="/redmine/projects/r302-s14-g14/repository/diff/sg1/sybil/protocol/sybil_server_udp_bin_protocol.py" method="get"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div>
+<table class="list changesets">
+<thead><tr>
+<th>#</th>
+<th></th>
+<th></th>
+<th>Date</th>
+<th>Author</th>
+<th>Comment</th>
+</tr></thead>
+<tbody>
+<tr class="changeset odd">
+<td class="id">
+  <a href="/redmine/projects/r302-s14-g14/repository/revisions/12" title="Revision 12">12</a>
+</td><td class="checkbox"><input checked="checked" id="cb-1" name="rev" onclick="$(&#x27;#cbto-2&#x27;).attr(&#x27;checked&#x27;,true);" type="radio" value="12" /></td>
+<td class="checkbox"></td>
+<td class="committed_on">02/16/2014 04:36 pm</td>
+<td class="author">Ismail ELOUAFIQ</td>
+<td class="comments"><p>TP1 ex2.. done!</p></td>
+</tr>
+<tr class="changeset even">
+<td class="id">
+  <a href="/redmine/projects/r302-s14-g14/repository/revisions/11" title="Revision 11">11</a>
+</td><td class="checkbox"><input id="cb-2" name="rev" onclick="$(&#x27;#cbto-3&#x27;).attr(&#x27;checked&#x27;,true);" type="radio" value="11" /></td>
+<td class="checkbox"><input checked="checked" id="cbto-2" name="rev_to" onclick="if ($(&#x27;#cb-2&#x27;).attr(&#x27;checked&#x27;)) {$(&#x27;#cb-1&#x27;).attr(&#x27;checked&#x27;,true);}" type="radio" value="11" /></td>
+<td class="committed_on">02/14/2014 05:16 pm</td>
+<td class="author">Ismail ELOUAFIQ</td>
+<td class="comments"><p>ex 1 done</p></td>
+</tr>
+<tr class="changeset odd">
+<td class="id">
+  <a href="/redmine/projects/r302-s14-g14/repository/revisions/8" title="Revision 8">8</a>
+</td><td class="checkbox"><input id="cb-3" name="rev" onclick="$(&#x27;#cbto-4&#x27;).attr(&#x27;checked&#x27;,true);" type="radio" value="8" /></td>
+<td class="checkbox"><input id="cbto-3" name="rev_to" onclick="if ($(&#x27;#cb-3&#x27;).attr(&#x27;checked&#x27;)) {$(&#x27;#cb-2&#x27;).attr(&#x27;checked&#x27;,true);}" type="radio" value="8" /></td>
+<td class="committed_on">02/14/2014 02:28 pm</td>
+<td class="author">Ismail ELOUAFIQ</td>
+<td class="comments"><p>ex 1 done</p></td>
+</tr>
+<tr class="changeset even">
+<td class="id">
+  <a href="/redmine/projects/r302-s14-g14/repository/revisions/1" title="Revision 1">1</a>
+</td><td class="checkbox"></td>
+<td class="checkbox"><input id="cbto-4" name="rev_to" onclick="if ($(&#x27;#cb-4&#x27;).attr(&#x27;checked&#x27;)) {$(&#x27;#cb-3&#x27;).attr(&#x27;checked&#x27;,true);}" type="radio" value="1" /></td>
+<td class="committed_on">02/06/2014 12:19 am</td>
+<td class="author">Alberto Blanc</td>
+<td class="comments"><p>added skeleton files</p></td>
+</tr>
+</tbody>
+</table>
+<input type="submit" value="View differences" />
+</form>
+
+
+
+        
+        <div style="clear:both;"></div>
+    </div>
+</div>
+</div>
+
+<div id="ajax-indicator" style="display:none;"><span>Loading...</span></div>
+<div id="ajax-modal" style="display:none;"></div>
+
+<div id="footer">
+  <div class="bgl"><div class="bgr">
+    Powered by <a href="http://www.redmine.org/">Redmine</a> &copy; 2006-2013 Jean-Philippe Lang
+  </div></div>
+</div>
+</div>
+</div>
+
+</body>
+</html>
